6df6e4ba25c9db63e41ff7ffe47c30872959b85e
alexwortega <alexdragannikolich@gmail.com>
2019-11-07 01:53:45 +0300

    gg

---
+++ Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Cube_move;
+    void Start()
+    {
+
+    }
+    public void move()
+    {
+        transform.Translate(1, 0, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Move the cube if the screen has the finger moving.
+            if (touch.phase == TouchPhase.Began)
+            {
+                move();
+            }
+        }
+    }
+}
